fa35fbceae3a13596d6f663029be1bd89c56356b
Dean Stephens <dean.g.stephens@gmail.com>
2023-07-31 14:12:56 +1000

    Add peerjs

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import {Duck, DuckProps, duckWidth} from "@/app/components/duck";
 
 export default function Home() {
     const [duckName, setDuckName] = useState<string>("");
+    const [serverPeerId, setServerPeerId] = useState<string>("");
     const [ducks, setDucks] = useState<DuckProps[]>([]);
     const [tickRate, setTickRate] = useState(100);
     const [distanceRequired, setDistanceRequired] = useState(1000);
@@ -55,12 +56,40 @@ export default function Home() {
         });
     }
 
+    const createServer = () => {
+        const peer = new Peer();
+        peer.on('open', function(id: string) {
+            console.log('My peer ID is: ' + id);
+        });
+
+        peer.on('connection', function(conn: any) {
+            conn.on('data', function(data: any){
+                // Will print 'hi!'
+                console.log(data);
+            });
+        });
+    }
+
+    const connectToServer = () => {
+        const peer = new Peer();
+
+        const conn = peer.connect(serverPeerId);
+
+        console.log("connecting to server", serverPeerId)
+
+        conn.on('open', function(){
+            console.log("sending message")
+            // here you have conn.id
+            conn.send('hi!');
+        });
+    }
+
     return (
         <div>
             <div className="grid grid-cols-4 gap-4">
                 <input type="text" id="first_name"
                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                       placeholder="John" required
+                       placeholder="Enter duck name" required
                        value={duckName}
                        onChange={e => setDuckName(e.target.value)}
                 ></input>
@@ -72,6 +101,19 @@ export default function Home() {
                 </button>
                 <button onClick={resetRace}
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Reset Race
+                </button>
+
+                <button onClick={createServer}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create Server
+                </button>
+                <input type="text" id="first_name"
+                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                       placeholder="Enter server id" required
+                       value={serverPeerId}
+                       onChange={e => setServerPeerId(e.target.value)}
+                ></input>
+                <button onClick={connectToServer}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Connect to Server
                 </button>
             </div>
             {ducks.map((duck, i) => (
